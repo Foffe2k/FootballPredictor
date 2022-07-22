7f547946f6d4cf73f4aa0105bc35b66818bfd6a2
Joakim Cederlöv <foffe2k@hotmail.com>
2022-07-22 11:03:55 +0200

    Minor formatting

--- TeamScore.cs
+++ TeamScore.cs
@@ -6,10 +6,9 @@ namespace FootballPredictor
 {
     class TeamScore
     {
-        public int score { get; private set; }
-        
+        public int score { get; private set; }        
         public Team team { get; private set; }
-
+        
         public TeamScore(int score, Team team)
         {
             this.score = score;
